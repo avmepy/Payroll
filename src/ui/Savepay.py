8cd38b79584768d8487aae17f53a5ad777ba09c1
avmepy <valentyn.kofanov@knu.ua>
2019-11-29 01:51:14 +0200

    init commit

--- src/ui/Savepay.py
+++ src/ui/Savepay.py
@@ -3,11 +3,6 @@
 # author: Valentyn Kofanov
 
 
-#!/usr/bin/env python3
-# -*-encoding: utf-8-*-
-# author: Valentyn Kofanov
-
-
 from kivy.lang import Builder
 from kivy.uix.screenmanager import Screen
 import openpyxl
